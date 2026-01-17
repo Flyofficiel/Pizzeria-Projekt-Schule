38fec1e4850a235cc21d35f5b74c8ad36da9b536
Fly <lucashuber667@gmail.com>
2026-01-17 15:52:13 +0100

    Update SQLdatabase.sql

--- SQLdatabase.sql
+++ SQLdatabase.sql
@@ -20,7 +20,7 @@ Create table Bestellungen (
 );
 insert into speisen (speisename, preis, zutaten) values ('Pizza Margherita', 8.50, 'Tomaten, Käse');
 insert into Bestellungen (bestellnr, speisename_fk) values (1, 'Pizza Margherita');
-
+insert into speisen (speisename, preis, zutaten) values ('Pizza Salami',8.50,'salami, tomaten, käse');
 CREATE VIEW Bestellungsübersicht AS
 SELECT 
     B.bestellnr, 
