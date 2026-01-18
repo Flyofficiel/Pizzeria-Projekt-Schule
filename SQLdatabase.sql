03e34ccaea3328751b570e0cd9f2e67a0a24825c
Lucas Huber <Lucashuber667@gmail.com>
2026-01-18 17:19:07 +0100

    update database

--- SQLdatabase.sql
+++ SQLdatabase.sql
@@ -18,6 +18,43 @@ Create table Bestellungen (
     foreign key(speisename_fk) references speisen (speisename)
     
 );
+
+create table mitarbeiter(
+personalnr int not null primary key,
+name varchar(100),
+berreich varchar(100)
+);
+
+-- 1. Tabelle Tische korrigieren
+Create table tische (
+    tisch int not null,
+    slot int not null,
+    datum datetime not null,
+    bestellnr_fk int,
+    -- Wir machen die Kombination aus Tisch, Slot und Datum zum Primärschlüssel
+    primary key (tisch, slot, datum),
+    foreign key(bestellnr_fk) references Bestellungen (bestellnr)
+);
+
+-- 2. Tabelle Reservierungen korrigieren
+create table reservierungen(
+    gastname varchar(100) primary key,
+    tisch_fk int,
+    slot_fk int,
+    datum_fk datetime,
+
+    -- Ein Fremdschlüssel, der auf alle drei Spalten gleichzeitig verweist
+    foreign key(tisch_fk, slot_fk, datum_fk) references tische (tisch, slot, datum)
+);
+
+create table rechnungen(
+rechnungsnr int not null unique primary key,
+bestellnr_fk int,
+gesamtpreis double,
+
+foreign key(bestellnr_fk) references Bestellungen (bestellnr)
+);
+
 insert into speisen (speisename, preis, zutaten) values ('Pizza Margherita', 8.50, 'Tomaten, Käse');
 insert into Bestellungen (bestellnr, speisename_fk) values (1, 'Pizza Margherita');
 
